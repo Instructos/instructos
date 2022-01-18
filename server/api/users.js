f26d45a8f4f58a16baca750f668a083d4ea05573
Trang Nguyen <ntttrang2130@gmail.com>
2022-01-18 18:44:36 -0500

    passport implementation

--- server/api/users.js
+++ server/api/users.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+const adminAuth = require('../auth/adminMiddleware')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', adminAuth, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      attributes: ['id', 'email', 'isAdmin']
     })
     res.json(users)
   } catch (err) {
